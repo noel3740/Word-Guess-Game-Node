17b0444f4bbb7aebee1c3fd8eeb6fde9a8a07657
Noel Nevarez <noel3740@gmail.com>
2018-12-14 16:22:09 -0600

    Adding play again prompt

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ console.log("Welcome to the Hawaiian themed word guess game!!!");
 console.log("=================================================");
 
 //Setup an array with all the words the user can guess from
-const words = ["ukulele", "aloha", "poke", "mahalo", "hawaii", "maui", "surf", 
-                "turtles", "whales", "Oahu", "Honolulu", "Waikiki", "volcano", 
-                "beaches", "harbor", "waterfalls", "mountains", "dolphins", "hula", 
-                "leis"];
+const words = ["ukulele", "aloha", "poke", "mahalo", "hawaii", "maui", "surf",
+    "turtles", "whales", "Oahu", "Honolulu", "Waikiki", "volcano",
+    "beaches", "harbor", "waterfalls", "mountains", "dolphins", "hula",
+    "leis"];
 
 let wordsToGuessFrom;
 let numGuessRemain;
 let wordToGuess;
-let lettersGuessed = [];
+let lettersGuessed;
+let wins = 0;
+let losses = 0;
 
 //Function to start a new game
 startNewGame = () => {
     numGuessRemain = 12;
+    lettersGuessed = [];
 
     //If wordsToGuessFrom is empty the repopulate with words from the constant variable words
     if (!wordsToGuessFrom || wordsToGuessFrom.length === 0) {
@@ -39,13 +42,26 @@ startNewGame = () => {
 promptGuess = () => {
 
     if (numGuessRemain === 0) {
-        console.log("No more guesses remaining!");
+        losses++;
+        console.log();
+        console.log('\x1b[31m%s\x1b[0m',"===============================================================");
+        console.log('\x1b[31m%s\x1b[0m',`You have lost!!! The correct word was: ${wordToGuess.wordToGuess}`);
+        console.log('\x1b[31m%s\x1b[0m',"===============================================================");
+        console.log();
+        //Prompt user to see if they want to play another round
+        playAgainPrompt();
         return;
     }
 
-    console.log("Current Word: ", wordToGuess.toString());
+    console.log();
+    console.log('\x1b[33m%s\x1b[0m', `WINS: ${wins}`);
+    console.log('\x1b[33m%s\x1b[0m', `LOSSES: ${losses}`);
+    console.log('\x1b[35m%s\x1b[0m', `CURRENT WORD: ${wordToGuess.toString()}`);
+    console.log();
     console.log("Number of guesses remaining: ", numGuessRemain);
     console.log("Letters already guessed: ", lettersGuessed.toString());
+    console.log();
+    console.log("==============================");
 
     inquirer.prompt([
         {
@@ -54,25 +70,72 @@ promptGuess = () => {
             validate: validateUserLetterGuess
         }
     ]).then(answer => {
-        numGuessRemain--;
         lettersGuessed.push(answer.guess);
         wordToGuess.guessLetterInWord(answer.guess);
-        console.log("==============================");
-        promptGuess();
+
+        //If there are still underscores in the word then the user has not guessed correctly yet
+        //Prompt the user to guess another letter
+        if (wordToGuess.toString().indexOf("_") >= 0) {
+            console.log("==============================");
+            promptGuess();
+        }
+        //User has guessed the word correctly
+        else {
+            userWins();
+        }
     });
 };
 
+//Function that runs when the user wins
+userWins = () => {
+    wins++;
 
+    console.log();
+    console.log('\x1b[32m%s\x1b[0m',"===============================================================");
+    console.log('\x1b[32m%s\x1b[0m',`You have won!!! The correct word was: ${wordToGuess.wordToGuess}`);
+    console.log('\x1b[32m%s\x1b[0m',"===============================================================");
+    console.log();
+
+    //Prompt user to see if they want to play another round
+    playAgainPrompt();
+};
+
+//Function to prompt user to see if they want to play another round
+playAgainPrompt = () => {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "playAgain",
+            message: "Would you like to play again?",
+            default: "y"
+        }
+    ]).then(answer => {
+        //If the user wants to play again then start a new game otherwise display the final results
+        if (answer.playAgain) {
+            console.log("===============================================================");
+            console.log();
+
+            startNewGame();
+        } else {
+            console.log("WINS: ", wins);
+            console.log("LOSSES: ", losses);
+        }
+    });
+};
 
 //Function that will validate the user input when the guess a letter
 validateUserLetterGuess = (guess) => {
     //The user's guess can only be one character in length
     if (guess.length !== 1) {
         return "Answer must but 1 character in length!";
-    } 
+    }
     //The user's guess cannot be one that was already guessed
     else if (lettersGuessed.indexOf(guess) >= 0) {
         return "Letter has already been guessed. Please select another letter.";
+    }
+    //Only allow letters
+    else if (!(/^[a-zA-Z]+$/.test(guess))) {
+        return "Only letters are allowed!";
     }
     //Passed all validation
     else {
